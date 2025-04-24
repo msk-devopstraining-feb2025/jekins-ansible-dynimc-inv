b63c3e3808bfca1c519fa1a025b75bb40ed755a7
MurageshSK12 <murageshkabbinakantimath@gmail.com>
2025-04-24 10:59:10 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,12 +2,13 @@ provider "aws"{
 region = "ap-south-1"
 }
 
-resource "aws_instance" "AWSEC2Instance"{
- ami = "ami-0f1dcc636b69a6438"
- instance_type = "t2.micro"
- key_name = "devopsjan2025"
- security_groups = ["launch-wizard-2"]
- tags={
-   Name = "TerraformServer"
- }
+resource "aws_instance" "AWSInstance" {
+     ami = "ami-0f1dcc636b69a6438"
+	 instance_type = "t2.micro"
+	 key_name = "MSK_New" 
+	 security_groups = ["launch-wizard-3" ]
+	 tags = {
+	 Name = "tomcatserver"
+	 }
 }
+
